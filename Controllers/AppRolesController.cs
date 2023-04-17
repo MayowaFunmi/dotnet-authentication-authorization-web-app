aa813a99654c92b6395af95f5c4225d1f916277c
Akinade Mayowa <akinade.mayowa@gmail.com>
2023-04-17 04:30:45 +0100

    first commit

---
+++ Controllers/AppRolesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace SalesCRMApp.Controllers
+{
+    public class AppRolesController : Controller
+    {
+        // define a _roleManager variable
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        // create a constructor and initialize the roleManager
+        public AppRolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        // list all roles created by users
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole model)
+        {
+            // check if role already exists
+            if (!_roleManager.RoleExistsAsync(model.Name).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(model.Name)).GetAwaiter().GetResult();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using SalesCRMApp.Data;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesCRMCnn")));
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>().AddDefaultTokenProviders()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages(); //added for identity services: register, login, logout ... with the prtial tag in _Layout.cshtml
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
